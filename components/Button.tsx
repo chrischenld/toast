af2bb9bb701a0f823916df771ea03d2419401189
chris <32497898+chrischenld@users.noreply.github.com>
2023-07-23 06:36:52 +0000

    fix(Button): remove unused "icon" prop, update Button component to remove commented-out code

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,6 @@
-export function Button({ text, onClick, type, icon }) {
+import React from "react";
+
+export function Button({ text, onClick, type }) {
   return (
     <button
       className="h-8 pl-2 pr-2 flex items-center gap-0.5 inline-block max-w-max rounded-md text-zinc-400 hover:text-zinc-200 focus:text-zinc-200 focus:outline-0 focus:ring-zinc-200 focus:ring-2 underline"
@@ -6,7 +8,6 @@ export function Button({ text, onClick, type, icon }) {
       type={type}
     >
       {text}
-      {/* {icon && <Icon name={icon} color="text-zinc-200" />} */}
     </button>
   );
 }
