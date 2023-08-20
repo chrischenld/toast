62621cdeb176ff2c2bad9cf0dd9eb8762a183b7e
chris <32497898+chrischenld@users.noreply.github.com>
2023-08-20 02:10:30 +0000

    feat(Sidebar):  refactored to support sub nav

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,12 +1,24 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { NavItem } from "./NavItem";
 import { Button } from "./Button";
-import { motion, AnimatePresence, useSpring } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import Link from "next/link";
 
-export function Sidebar() {
+interface Props {
+  hasSubNav: boolean;
+  subNavItems: SubNavItems[];
+}
+
+interface SubNavItems {
+  name: string;
+  href: string;
+  accent?: boolean;
+  accentText?: string;
+}
+
+export function Sidebar({ hasSubNav, subNavItems }: Props) {
   const [sidebarCollapse, setSidebarCollapse] = useState("18rem");
   const [hideCollapsed, setHideCollapsed] = useState(true);
 
@@ -17,38 +29,80 @@ export function Sidebar() {
   };
 
   return (
-    <motion.div
-      className={`flex flex-col gap-4 max-h-screen p-4 bg-default-base border-r-2 border-default-border`}
-      initial={{ width: sidebarCollapse }} // Animate the width change
-      animate={{ width: sidebarCollapse }} // Animate the width change
-      transition={{
-        type: "spring",
-        stiffness: 700,
-        damping: 50,
-      }}
-    >
-      <Button text="hide" onClick={toggleSidebarCollapse} />
-      <AnimatePresence>
-        {hideCollapsed && (
+    <div className="flex flex-row">
+      <motion.div
+        className={`flex flex-col gap-4 max-h-screen p-4 bg-default-base border-r-2 border-default-border`}
+        initial={{ width: sidebarCollapse }} // Animate the width change
+        animate={{ width: sidebarCollapse }} // Animate the width change
+        transition={{
+          type: "spring",
+          stiffness: 700,
+          damping: 50,
+        }}
+      >
+        <Button text="hide" onClick={toggleSidebarCollapse} />
+        <AnimatePresence>
+          {hideCollapsed && (
+            <motion.div
+              className="flex flex-col gap-1"
+              initial={{ opacity: 0, x: "-1rem" }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: "-1rem" }}
+              transition={{ type: "spring", stiffness: 500, damping: 50 }}
+            >
+              <Link href="/">
+                <NavItem itemName="home" />
+              </Link>
+              <Link href="/toaster">
+                <NavItem itemName="toaster" />
+              </Link>
+              <Link href="/flyout">
+                <NavItem itemName="flyout" accent accentText="new" />
+              </Link>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </motion.div>
+      {hasSubNav && (
+        <motion.div
+          className="flex flex-col gap-4 max-h-screen p-4 bg-default-base border-r-2 border-default-border"
+          initial={{ opacity: 0, width: "2rem" }}
+          animate={{ opacity: 1, width: "18rem" }}
+          transition={{
+            type: "spring",
+            stiffness: 700,
+            damping: 50,
+            delay: 0.03,
+          }}
+        >
           <motion.div
-            className="flex flex-col gap-1"
+            className="flex flex-col gap-1 pt-12"
             initial={{ opacity: 0, x: "-1rem" }}
             animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: "-1rem" }}
-            transition={{ type: "spring", stiffness: 500, damping: 50 }}
+            exit={{ opacity: 0.5, x: "-1rem" }}
+            transition={{
+              type: "spring",
+              stiffness: 500,
+              damping: 50,
+              delay: 0.03,
+            }}
           >
-            <Link href="/">
-              <NavItem itemName="home" />
-            </Link>
-            <Link href="/toaster">
-              <NavItem itemName="toaster" />
-            </Link>
-            <Link href="/flyout">
-              <NavItem itemName="flyout" accent accentText="new" />
-            </Link>
+            {subNavItems.map((item, index) => (
+              <Link key={index} href={item.href}>
+                <NavItem
+                  itemName={item.name}
+                  accent={item.accent}
+                  accentText={item.accentText}
+                />
+              </Link>
+            ))}
           </motion.div>
-        )}
-      </AnimatePresence>
-    </motion.div>
+        </motion.div>
+      )}
+    </div>
   );
 }
+
+Sidebar.defaultProps = {
+  hasSubNav: false,
+};
